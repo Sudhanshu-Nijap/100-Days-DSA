2b1b85d983ff982bf73c3232eb443aeb52ca84c1
Sudhu2005 <sudhanshun10b3720@gmail.com>
2024-11-03 23:42:23 +0530

    is subsequence

---
+++ Array/issubsequence.py
@@ -0,0 +1,15 @@
+def isSubsequence(s: str, t: str) -> bool:
+        S,T = len(s),len(t)
+        if s == '':
+            return True
+        if S>T:
+            return False
+        j = 0
+        for i in range(T):
+            if t[i] == s[j]:
+                if j == S-1:
+                    return True
+                j+=1
+        return False
+
+print(isSubsequence('abc','ahbqwec'))
