f6efbd8da650788d98aef17c22ebd7ef5c4eeb29
Sudhu2005 <sudhanshun10b3720@gmail.com>
2024-11-04 21:52:23 +0530

    two sum

--- Array/twosum.py
+++ Array/twosum.py
@@ -6,3 +6,16 @@ def twosum(arr,target):
             if arr[i] + arr[j] == target:
                 return i,j
 print(twosum([2,7,11,15],9))
+
+#Better
+def twosum(arr,target):
+    n = len(arr)
+    h = {}
+    for i in range(n):
+        h[arr[i]] = i
+    
+    for i in range(n):
+        y = target - arr[i]
+        if y in h and h[y] != i:
+            return [i,h[y]]
+print(twosum([2,7,11,15],9))
