73cc70db988d9ab4982a67019f08ac1f50cc64ef
Sudhu2005 <sudhanshun10b3720@gmail.com>
2024-11-14 23:28:40 +0530

    prefix

---
+++ Array/Prefix Sum Technique/problem1.py
@@ -0,0 +1,6 @@
+def prefix(arr,query)->list:
+    n = len(arr)
+    for i in range(1,n):
+        arr[i]+=arr[i-1]
+    return (list(arr[r]-arr[l-1] for l,r in query))
+print(prefix([1,2,3,4,5],[(1,3),(2,4)]))
